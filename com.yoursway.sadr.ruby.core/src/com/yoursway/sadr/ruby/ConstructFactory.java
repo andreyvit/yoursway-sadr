94267bb26360d5e2f53907e2ec973e38e7987d26
Andrey Tarantsov <andreyvit@gmail.com>
2008-01-31 07:18:34 +0600

    Handled all existing nodes in the Ruby's ConstructFactory.

--- com.yoursway.sadr.ruby.core/src/com/yoursway/sadr/ruby/ConstructFactory.java
+++ com.yoursway.sadr.ruby.core/src/com/yoursway/sadr/ruby/ConstructFactory.java
@@ -1,13 +1,17 @@
 package com.yoursway.sadr.ruby;
 
 import org.eclipse.dltk.ast.ASTNode;
+import org.eclipse.dltk.ast.declarations.MethodDeclaration;
 import org.eclipse.dltk.ast.declarations.ModuleDeclaration;
 import org.eclipse.dltk.ast.expressions.CallExpression;
+import org.eclipse.dltk.ast.statements.Block;
 
 import com.yoursway.sadr.core.Construct;
 import com.yoursway.sadr.core.Scope;
 import com.yoursway.sadr.ruby.constructs.CallC;
+import com.yoursway.sadr.ruby.constructs.MethodDeclarationC;
 import com.yoursway.sadr.ruby.constructs.ModuleDeclarationC;
+import com.yoursway.sadr.ruby.constructs.StatementListC;
 
 public class ConstructFactory {
     
@@ -16,9 +20,13 @@ public class ConstructFactory {
     public Construct create(Scope scope, ASTNode node) {
         if (node instanceof ModuleDeclaration)
             return new ModuleDeclarationC(scope, (ModuleDeclaration) node);
-        else if (node instanceof CallExpression)
+        if (node instanceof MethodDeclaration)
+            return new MethodDeclarationC(scope, (MethodDeclaration) node);
+        if (node instanceof CallExpression)
             return new CallC(scope, (CallExpression) node);
-        return null;
+        if (node instanceof Block)
+            return new StatementListC(scope, node);
+        throw new RuntimeException("No construct found for node " + node.getClass());
     }
     
 }
