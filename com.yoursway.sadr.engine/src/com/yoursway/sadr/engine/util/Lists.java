5103755f6a3c85e63cc145a7f5d77cc3ab05633c
Andrey Tarantsov <andreyvit@gmail.com>
2008-03-01 08:30:53 +0600

    Added dependency on Google Collections framework.

--- com.yoursway.sadr.engine/src/com/yoursway/sadr/engine/util/Lists.java
+++ com.yoursway.sadr.engine/src/com/yoursway/sadr/engine/util/Lists.java
@@ -1,10 +1,15 @@
 package com.yoursway.sadr.engine.util;
 
+import static com.google.common.collect.Lists.newArrayListWithCapacity;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterators;
 
 public class Lists {
     
@@ -20,6 +25,23 @@ public class Lists {
         allNodes.addAll(a);
         allNodes.addAll(b);
         return allNodes;
+    }
+    
+    public static <T> List<T> filterInto(Collection<? extends T> source, List<T> storage,
+            Predicate<? super T> pred) {
+        Iterators.addAll(storage, Iterators.filter(source.iterator(), pred));
+        return storage;
+    }
+    
+    public static <T> Collection<T> filterInto(Collection<? extends T> source, Collection<T> storage,
+            Predicate<? super T> pred) {
+        Iterators.addAll(storage, Iterators.filter(source.iterator(), pred));
+        return storage;
+    }
+    
+    public static <T> List<T> filter(Collection<? extends T> source, Predicate<? super T> pred) {
+        List<T> storage = newArrayListWithCapacity(source.size());
+        return filterInto(source, storage, pred);
     }
     
 }
