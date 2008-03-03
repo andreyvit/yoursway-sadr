25e948eee73a7a25e87da3e7dcac181d8e5b0a8e
Andrey Tarantsov <andreyvit@gmail.com>
2008-03-04 01:25:05 +0600

    Python: Fixed incorrect BackPropagationTests/unusedArgumentIsNotEvaluated.

--- com.yoursway.sadr.python.core.tests/tests/BackPropagationTests/unusedArgumentIsNotEvaluated/foo.py
+++ com.yoursway.sadr.python.core.tests/tests/BackPropagationTests/unusedArgumentIsNotEvaluated/foo.py
@@ -6,8 +6,8 @@ def bozz():
   a = 1
   b = 2
   z = bar(a, b)
-  print a	;; not-cached localvar-type a
-  print b	;; not-cached localvar-type b
-  print z	;; expr z => int
-  print a	;; not-cached localvar-type a
-  print b	;; cached localvar-type b
+  print a	## not-cached localvar-type a
+  print b	## not-cached localvar-type b
+  print z	## expr z => int
+  print a	## not-cached localvar-type a
+  print b	## cached localvar-type b
