8b8528b1195adc17dc3b71d9bb9d7e7c485da30f
Andrey Tarantsov <andreyvit@gmail.com>
2008-03-16 00:28:25 +0600

    Python: Added a hardcore test. :-)

---
+++ com.yoursway.sadr.python.core.tests/tests/HardcoreTests/instanceTracking/foo.py
@@ -0,0 +1,26 @@
+
+class Bar:
+  pass
+  
+class Xxx:
+  pass
+  
+class Moo:
+  def __init__(self, p):
+    self.bbb = p.q
+    
+  def boo(self):
+    return self.ccc
+    
+mmm = Moo("s")
+mmm.ccc = "bbbbbb"
+
+s = Bar()
+xxx = Xxx()
+s.boz = xxx
+moo = Moo(s)
+moo.ccc = moo.bbb
+bar = moo.boo()
+xxx.biz = 42
+foo = bar.boz.biz
+print foo ## expr foo => int
