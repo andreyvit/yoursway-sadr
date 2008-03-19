bc4afbf6e7b1686c9b0456e00d2f0d5ca0c0ea34
unknown <@.(none)>
2008-03-19 11:45:30 +0600

    subclassing test source fix

--- com.yoursway.sadr.python.core.tests/tests/VariablesTests/subclassing/foo.py
+++ com.yoursway.sadr.python.core.tests/tests/VariablesTests/subclassing/foo.py
@@ -1,17 +1,14 @@
-
 class Foo(object):
 	def __init__(self, x):
 		self.a = x
 		
 class Bar(Foo):
-	def __init__(self, y):
-		Foo.__init__(self, 5)
-		self.y = a
+	def __init__(self, buzz):
+		Foo.__init__(self, buzz)
+		self.y = self.a
 
 def foo():
-	q = Bar().y ## expr q => int
+	q = Bar(5).y ## expr q => int
 	print q ## value q => 5
-	w = Bar().a ## expr w => int
+	w = Bar(5).a ## expr w => int
 	print w ## value w => 5
- 
-		
