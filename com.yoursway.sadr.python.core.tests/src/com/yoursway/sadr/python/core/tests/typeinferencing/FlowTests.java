158a1d75500d1d02e37861630b116e09889f5cbd
unknown <@.(none)>
2008-03-19 14:07:16 +0600

    Simple flow test

---
+++ com.yoursway.sadr.python.core.tests/src/com/yoursway/sadr/python/core/tests/typeinferencing/FlowTests.java
@@ -0,0 +1,10 @@
+package com.yoursway.sadr.python.core.tests.typeinferencing;
+
+import org.junit.Test;
+
+public final class FlowTests extends AbstractTypeInferencingTestCase {
+    @Test
+    public void flowTest() throws Exception {
+        runTest();
+    }
+}
