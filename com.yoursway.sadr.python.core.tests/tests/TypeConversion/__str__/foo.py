bd884c151b020ac4c82ede624e899cbee02d385d
Leontiy S. Deriglazov <leon@host-10-199-49-7.academ.local>
2008-03-30 00:07:30 +0600

    test_gen fixed (different behaviour of os.mkdir on linux and win32);\ntype conversion tests added

---
+++ com.yoursway.sadr.python.core.tests/tests/TypeConversion/__str__/foo.py
@@ -0,0 +1,6 @@
+
+class Foo(object):
+    def __str__ (self):
+        return string
+x = str(Foo()) ## value x => string
+
--- com.yoursway.sadr.python.core.tests/tests/test_gen.py
+++ com.yoursway.sadr.python.core.tests/tests/test_gen.py
@@ -1,5 +1,6 @@
 import os
 
+JUNIT_TESTS_PATH = "../src/com/yoursway/sadr/python/core/tests/typeinferencing/"
 FILE_NAME = "foo.py"
 
 JAVA_TEST_HEAD = """package com.yoursway.sadr.python.core.tests.typeinferencing;
@@ -22,20 +23,22 @@ def mkdir(dir):
         os.mkdir(dir)
 
 class TestBuilder(object):
-    def __init__(self, path, suite_name):
+    def __init__(self, suite_name, path = JUNIT_TESTS_PATH):
         self.java_test_file = open(path + suite_name + ".java", "w")
         self.suite_name = suite_name
         mkdir(path)
+        mkdir(suite_name)
         print >>self.java_test_file, JAVA_TEST_HEAD % suite_name
         
-    def __make_python_test(self, suite_name, test_name, content):
-        mkdir(suite_name + "/" + test_name)
-        file = open(suite_name + "/" + test_name + "/" + FILE_NAME, "w")
+    def __make_python_test(self, test_name, content):
+        path = self.suite_name + "/" + test_name + "/"
+        mkdir(path)
+        file = open(path + FILE_NAME, "w")
         print >> file, content
     
     def addTest(self, test_name, script_content):
         print >>self.java_test_file, JAVA_TEST_BODY % test_name
-        self.__make_python_test(self.suite_name, test_name, script_content)
+        self.__make_python_test(test_name, script_content)
 
     def __del__(self):
         print >> self.java_test_file, JAVA_TEST_TAIL
