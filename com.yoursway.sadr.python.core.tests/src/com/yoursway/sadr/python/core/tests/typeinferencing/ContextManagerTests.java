17bae0a3f13f425714fb0b3963ba23439d1942ab
Leontiy Deriglazov <LeontyD@gmail.com>
2008-04-03 15:41:38 +0700

    OBSOLETES previous commit; Context manager test added

---
+++ com.yoursway.sadr.python.core.tests/src/com/yoursway/sadr/python/core/tests/typeinferencing/ContextManagerTests.java
@@ -0,0 +1,12 @@
+package com.yoursway.sadr.python.core.tests.typeinferencing;
+
+import org.junit.Test;
+
+public final class ContextManagerTests extends AbstractTypeInferencingTestCase {
+
+    @Test
+    public void contextManager() throws Exception {
+        runTest();
+    }
+
+}
---
+++ com.yoursway.sadr.python.core.tests/src/com/yoursway/sadr/python/core/tests/typeinferencing/DynamicFeaturesTests.java
@@ -0,0 +1,37 @@
+package com.yoursway.sadr.python.core.tests.typeinferencing;
+
+import org.junit.Test;
+
+public final class DynamicFeaturesTests extends AbstractTypeInferencingTestCase {
+
+    @Test
+    public void closure() throws Exception {
+        runTest();
+    }
+
+    @Test
+    public void instanceAttributeFromFunction() throws Exception {
+        runTest();
+    }
+
+    @Test
+    public void metaclassAttr() throws Exception {
+        runTest();
+    }
+
+    @Test
+    public void functionObjectFlows() throws Exception {
+        runTest();
+    }
+
+    @Test
+    public void calculatedSuperClasse() throws Exception {
+        runTest();
+    }
+
+    @Test
+    public void instanceMethodFromFunction() throws Exception {
+        runTest();
+    }
+
+}
