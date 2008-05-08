30f44aa95ab0f39c530d6b95761b7a82b7487393
Leontiy Deriglazov <LeontyD@gmail.com>
2008-05-08 16:47:53 +0700

    sync shit

---
+++ com.yoursway.sadr.python.core/src/com/yoursway/sadr/python_v2/goals/internal/AuxMethodNames.java
@@ -0,0 +1,4 @@
+package com.yoursway.sadr.python_v2.goals.internal;
+
+public final class AuxMethodNames {
+}
---
+++ com.yoursway.sadr.python.core/src/com/yoursway/sadr/python_v2/goals/internal/CallResolver.java
@@ -0,0 +1,17 @@
+package com.yoursway.sadr.python_v2.goals.internal;
+
+import com.yoursway.sadr.python_v2.model.RuntimeObject;
+import com.yoursway.sadr.python_v2.model.builtins.FunctionObject;
+import com.yoursway.sadr.succeeder.IGoal;
+
+public final class CallResolver {
+    public static IGoal callMethod(RuntimeObject receiver, String name, ArgumentsContext args) {
+        //TODO
+        return null;
+    }
+    
+    public static IGoal callFunction(FunctionObject callable, ArgumentsContext args) {
+        //TODO
+        return null;
+    }
+}
