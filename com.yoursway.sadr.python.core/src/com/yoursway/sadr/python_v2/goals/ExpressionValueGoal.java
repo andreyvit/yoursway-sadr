afcbda7d367b055e1be900d1524fda32ad626f61
Leontiy Deriglazov <LeontyD@gmail.com>
2008-05-08 16:46:48 +0700

    sync shit

--- com.yoursway.sadr.python.core/src/com/yoursway/sadr/python_v2/goals/ExpressionValueGoal.java
+++ com.yoursway.sadr.python.core/src/com/yoursway/sadr/python_v2/goals/ExpressionValueGoal.java
@@ -2,11 +2,14 @@ package com.yoursway.sadr.python_v2.goals;
 
 import org.eclipse.dltk.python.parser.ast.expressions.PythonVariableAccessExpression;
 
+import com.yoursway.sadr.python.Grade;
 import com.yoursway.sadr.python.core.runtime.contributions.Context;
 import com.yoursway.sadr.python.core.typeinferencing.constructs.BinaryC;
 import com.yoursway.sadr.python.core.typeinferencing.constructs.IntegerLiteralC;
 import com.yoursway.sadr.python.core.typeinferencing.constructs.PythonConstruct;
 import com.yoursway.sadr.python.core.typeinferencing.constructs.StringLiteralC;
+import com.yoursway.sadr.python_v2.goals.internal.CallResolver;
+import com.yoursway.sadr.python_v2.model.RuntimeObject;
 import com.yoursway.sadr.python_v2.model.builtins.IntType;
 import com.yoursway.sadr.python_v2.model.builtins.StringType;
 import com.yoursway.sadr.succeeder.CheckpointToken;
@@ -29,21 +32,34 @@ public class ExpressionValueGoal extends ContextSensitiveGoal {
     }
     
     public void preRun() {
-        PythonValueSetAcceptor subgoalAcceptor = new PythonValueSetAcceptor() {
-            public void checkpoint(IGrade<?> grade) {
-                acceptor.setResult(getResult());
-            }
-        };
         if (expression instanceof IntegerLiteralC) {
             acceptor.addResult(IntType.newIntObject((IntegerLiteralC) expression), getContext());
         } else if (expression instanceof StringLiteralC) {
             acceptor.addResult(StringType.newStringObject((StringLiteralC) expression), getContext());
         } else if (expression instanceof PythonVariableAccessExpression) {
+            PythonValueSetAcceptor subgoalAcceptor = new PythonValueSetAcceptor() {
+                public void checkpoint(IGrade<?> grade) {
+                    acceptor.setResult(getResult());
+                }
+            };
             //TODO for Bur
             IGoal goal = new ResolveReference((PythonVariableAccessExpression) expression, subgoalAcceptor);
             schedule(goal);
         } else if (expression instanceof BinaryC) {
-            
+            BinaryC binExp = (BinaryC)expression;
+            BinaryAcceptor subgoalAcceptor = new BinaryAcceptor() {
+                public void checkpoint(IGrade<?> grade) {
+                    if (!getLeft().isEmpty() && !getRight().isEmpty()) {
+                        //TODO evaluate result for each context
+                        RuntimeObject receiver = getLeft().get(getContext());
+                        RuntimeObject argument = getRight().get(getContext());
+                        String name = binExp.node().getKind();
+                        CallResolver.callMethod(receiver, name, new Arguments(argument));
+                        ExpressionValueGoal.this.checkpoint(acceptor, Grade.DONE);
+                    }
+                }
+            };
+            schedule()
         }
     }
 }
