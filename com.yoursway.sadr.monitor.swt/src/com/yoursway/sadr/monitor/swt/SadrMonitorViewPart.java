24470f1c7185ac9b643d33b965a7cb7980af8cc8
Andrey Tarantsov <andreyvit@gmail.com>
2008-09-28 20:56:27 +0700

    SADR Monitor plugin created.

---
+++ com.yoursway.sadr.monitor.swt/src/com/yoursway/sadr/monitor/swt/SadrMonitorViewPart.java
@@ -0,0 +1,19 @@
+package com.yoursway.sadr.monitor.swt;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class SadrMonitorViewPart extends ViewPart {
+    
+    public SadrMonitorViewPart() {
+    }
+    
+    @Override
+    public void createPartControl(Composite parent) {
+    }
+    
+    @Override
+    public void setFocus() {
+    }
+    
+}
