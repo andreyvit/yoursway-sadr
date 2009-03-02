d1b9b878f272754b67f91a0afdc2665178384342
Yuri Baburov <burchik@gmail.com>
2009-03-02 10:04:05 +0600

    Updated to better classpaths & newer imports.

--- com.yoursway.sadr.python.idioms.core/src/com/yoursway/sadr/python/idioms/core/SnippetParser.java
+++ com.yoursway.sadr.python.idioms.core/src/com/yoursway/sadr/python/idioms/core/SnippetParser.java
@@ -1,9 +1,6 @@
 package com.yoursway.sadr.python.idioms.core;
 
-import org.eclipse.core.resources.IMarker;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.dltk.ast.declarations.ModuleDeclaration;
-import org.eclipse.dltk.ast.parser.ISourceParser;
 import org.eclipse.dltk.compiler.problem.IProblem;
 import org.eclipse.dltk.compiler.problem.IProblemReporter;
 import org.eclipse.dltk.python.internal.core.parser.PythonSourceParser;
@@ -11,17 +8,11 @@ import org.eclipse.dltk.python.internal.core.parser.PythonSourceParser;
 public class SnippetParser {
 
 	public static ModuleDeclaration parseSnippet(String source) {
-		ISourceParser parser = new PythonSourceParser();
+		PythonSourceParser parser = new PythonSourceParser();
 		ModuleDeclaration rootNode = parser.parse(null, source.toCharArray(),
 				new IProblemReporter() {
-	
-					public void clearMarkers() {
-					}
-	
-					public IMarker reportProblem(IProblem problem)
-							throws CoreException {
-						throw new RuntimeException("OMFG! "
-								+ problem.getMessage());
+					public void reportProblem(IProblem problem) {
+						throw new RuntimeException("OMFG!");
 					}
 	
 				});
