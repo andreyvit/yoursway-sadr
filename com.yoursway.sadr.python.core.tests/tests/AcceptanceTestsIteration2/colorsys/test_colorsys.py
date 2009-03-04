91950b6c4433664929f1c51f67e0adbc5437f3de
Yuri Baburov <burchik@gmail.com>
2009-03-04 15:51:39 +0600

    Fixed indentation & removed not used stuff.

--- com.yoursway.sadr.python.core.tests/tests/AcceptanceTestsIteration2/colorsys/test_colorsys.py
+++ com.yoursway.sadr.python.core.tests/tests/AcceptanceTestsIteration2/colorsys/test_colorsys.py
@@ -81,46 +81,25 @@ class ColorsysTest(unittest.TestCase):
             x += self.assertTripleEqual(rgb, colorsys.hls_to_rgb(*hls))
         return x
 
-        def test_simple(self):
-            values = [
-                # rgb, hls
-                ((0.0, 0.0, 0.0), (  0  , 0.0, 0.0)), # black
-                ((0.0, 0.0, 1.0), (4./6., 0.5, 1.0)), # blue
-                ((0.0, 1.0, 0.0), (2./6., 0.5, 1.0)), # green
-                ((0.0, 1.0, 1.0), (3./6., 0.5, 1.0)), # cyan
-                ((1.0, 0.0, 0.0), (  0  , 0.5, 1.0)), # red
-                ((1.0, 0.0, 1.0), (5./6., 0.5, 1.0)), # purple
-                ((1.0, 1.0, 0.0), (1./6., 0.5, 1.0)), # yellow
-                ((1.0, 1.0, 1.0), (  0  , 1.0, 0.0)), # white
-                ((0.5, 0.5, 0.5), (  0  , 0.5, 0.0)), # grey
-            ]
-            x = ""
-            rgb = values[0][0]
-            hls = values[0][1]
-            x += self.assertTripleEqual(hls, colorsys.rgb_to_hls(*rgb))
-            x += self.assertTripleEqual(rgb, colorsys.hls_to_rgb(*hls))
-            return x
-
-        
-            values = [
-                # rgb, hls
-                ((0.0, 0.0, 0.0), (  0  , 0.0, 0.0)), # black
-                ((0.0, 0.0, 1.0), (4./6., 0.5, 1.0)), # blue
-                ((0.0, 1.0, 0.0), (2./6., 0.5, 1.0)), # green
-                ((0.0, 1.0, 1.0), (3./6., 0.5, 1.0)), # cyan
-                ((1.0, 0.0, 0.0), (  0  , 0.5, 1.0)), # red
-                ((1.0, 0.0, 1.0), (5./6., 0.5, 1.0)), # purple
-                ((1.0, 1.0, 0.0), (1./6., 0.5, 1.0)), # yellow
-                ((1.0, 1.0, 1.0), (  0  , 1.0, 0.0)), # white
-                ((0.5, 0.5, 0.5), (  0  , 0.5, 0.0)), # grey
-            ]
-            x = ""
-            for (rgb, hls) in values:
-                x += self.assertTripleEqual(hls, colorsys.rgb_to_hls(*rgb))
-                x += self.assertTripleEqual(rgb, colorsys.hls_to_rgb(*hls))
-            return x
-
-            t = assertTripleEqual()
+    def test_simple(self):
+        values = [
+            # rgb, hls
+            ((0.0, 0.0, 0.0), (  0  , 0.0, 0.0)), # black
+            ((0.0, 0.0, 1.0), (4./6., 0.5, 1.0)), # blue
+            ((0.0, 1.0, 0.0), (2./6., 0.5, 1.0)), # green
+            ((0.0, 1.0, 1.0), (3./6., 0.5, 1.0)), # cyan
+            ((1.0, 0.0, 0.0), (  0  , 0.5, 1.0)), # red
+            ((1.0, 0.0, 1.0), (5./6., 0.5, 1.0)), # purple
+            ((1.0, 1.0, 0.0), (1./6., 0.5, 1.0)), # yellow
+            ((1.0, 1.0, 1.0), (  0  , 1.0, 0.0)), # white
+            ((0.5, 0.5, 0.5), (  0  , 0.5, 0.0)), # grey
+        ]
+        x = ""
+        rgb = values[0][0]
+        hls = values[0][1]
+        x += self.assertTripleEqual(hls, colorsys.rgb_to_hls(*rgb))
+        x += self.assertTripleEqual(rgb, colorsys.hls_to_rgb(*hls))
+        return x
 
 def test_main():
     cst = ColorsysTest()
