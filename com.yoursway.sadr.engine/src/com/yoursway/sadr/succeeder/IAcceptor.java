6ddf0164b199fbc8f11c09181fb6a07ccf099888
Andrey Tarantsov <andreyvit@gmail.com>
2009-04-04 17:05:01 +0700

    Removed unused "SADR succeeder" code from the engine.

--- com.yoursway.sadr.engine/src/com/yoursway/sadr/succeeder/IAcceptor.java
+++
@@ -1,7 +0,0 @@
-package com.yoursway.sadr.succeeder;
-
-import com.yoursway.sadr.engine.Result;
-
-public interface IAcceptor extends Result {
-    <T> void checkpoint(IGrade<T> grade);
-}
--- com.yoursway.sadr.engine/src/com/yoursway/sadr/succeeder/IGrade.java
+++
@@ -1,9 +0,0 @@
-package com.yoursway.sadr.succeeder;
-
-/**
- * Result readiness grade marker.
- */
-public interface IGrade<T> extends Comparable<T> {
-    
-    boolean isDone();
-}
